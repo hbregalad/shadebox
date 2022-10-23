bb4a98113ce55ef7b15639ad4f7bda4f9d640147
Hezekiah.Bregalad <hbregalad@gmail.com>
2022-10-23 10:43:35 -0400

    ok so systemctl wants a hard link, now what does it want in ExecStop?

--- setup.py
+++ setup.py
@@ -19,13 +19,14 @@ ConditionPathExists={0}
 
 [Service]
 Type=idle
-ExecStart=/usr/bin/nohup {2} {0} > {1} 2>&1
-Restart=on-failure
-ExecStop=/bin/kill -INT $MAINPID
+ExecStart=/usr/bin/nohup {2} {0}
 
 [Install]
 WantedBy=multi-user.target
 """
+# > {1} 2>&1
+#Restart=on-failure
+#ExecStop=/bin/kill -INT $MAINPID
 
 def which(command):
     ret =  subprocess.check_output(['which', command]).decode().rstrip()
