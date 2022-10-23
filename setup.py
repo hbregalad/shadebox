0fa562642f89e5213759b19966471fa8b30f6395
Hezekiah.Bregalad <hbregalad@gmail.com>
2022-10-22 22:16:18 -0400

    os.path.abspath()

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 "setup.py"
-import os
+import os#, sys
+#if __file__:
 
-MAIN = __file__.replace('setup.py', 'main.py')
-LOG = __file__.replace('setup.py', 'shadebox.log')
+#print(__file__)
+#print(sys.argv[0])
+#f = os.path.abspath(__file__)
+##print(f)
+##f = os.path.abspath(sys.argv[0])
+##print(f)
 
 SERVICE_DATA = """[Unit]
 Description=shadebox service
@@ -16,10 +21,23 @@ Type=idle
 ExecStart=/usr/bin/python3 {0} >> {1}
 
 [Install]
-WantedBy=multi-user.target""".format(MAIN, LOG)
+WantedBy=multi-user.target"""
 
-SERVICE = __file__.replace('setup.py', 'shadebox.service')
-with open(SERVICE, 'w') as f:
-    f.write(SERVICE_DATA)
+def make_service(file):
+    main = file.replace('setup.py', 'main.py')
+    log = file.replace('setup.py', 'shadebox.log')
+    service = file.replace('setup.py', 'shadebox.service')
 
-os.symlink(SERVICE, '/etc/systemd/system/shadebox.service')
+    with open(log, mode='w') as f:
+        f.write('log created')
+    
+    print(main, log, service, sep='\n')
+    
+    data = SERVICE_DATA.format(main, log)
+
+    with open(service, 'w') as f:
+        f.write(data)
+
+    os.symlink(service, '/etc/systemd/system/shadebox.service')
+
+make_service(os.path.abspath(__file__))
