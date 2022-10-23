be7c581e267c9c6db647cab78cd3ffbdeedb3595
Hezekiah.Bregalad <hbregalad@gmail.com>
2022-10-22 22:00:28 -0400

    tying in to systemd

---
+++ setup.py
@@ -0,0 +1,25 @@
+"setup.py"
+import sys
+
+MAIN = __file__.replace('setup.py', 'main.py')
+LOG = __file__.replace('setup.py', 'shadebox.log')
+
+SERVICE_DATA = """[Unit]
+Description=shadebox service
+After=multi-user.target
+Wants=systemd-timesyncd.service
+After=systemd-timesyncd.service
+ConditionPathExists={0}
+
+[Service]
+Type=idle
+ExecStart=/usr/bin/python3 {0} >> {1}
+
+[Install]
+WantedBy=multi-user.target""".format(MAIN, LOG)
+
+SERVICE = __file__.replace('setup.py', 'shadebox.service')
+with open(SERVICE, 'w') as f:
+    f.write(SERVICE_DATA)
+
+os.symlink(SERVICE, '/etc/systemd/system/shadebox.service')
